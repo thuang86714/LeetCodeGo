b34178ff6220aca9990eb662d124bf9d74ae7453
thuang86714 <thuang0714@gmail.com>
2023-05-16 00:20:27 +0800

    leetcode ~28, may/15 completed 10questions

---
+++ 61_Rotate_List.go
@@ -0,0 +1,40 @@
+/*
+Given the head of a linked list, rotate the list to the right by k places.
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+*/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ /**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func rotateRight(head *ListNode, k int) *ListNode {
+    if head ==nil || k==0{
+        return head
+    }
+    size := 1
+    tail, cur := head, head
+    for tail.Next !=nil{
+        size++
+        tail = tail.Next
+    }
+    k %= size
+    if k==0{return head}
+    for i:=0; i < size-k-1; i++{
+        cur = cur.Next
+    }
+    newhead := cur.Next 
+    cur.Next = nil
+    tail.Next=head
+    return newhead
+
+}
---
+++ 63_Unique_Paths2.go
@@ -0,0 +1,46 @@
+/*
+You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+
+Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The testcases are generated so that the answer will be less than or equal to 2 * 109.
+Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+Output: 2
+Explanation: There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
+*/
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	//credit to brianchiang_tw
+    m := len(obstacleGrid)
+    n := len(obstacleGrid[0])
+    var allowToVisit func (x, y int) int//in go, func could be a var as well
+    
+    allowToVisit = func(x, y int) int{//this helper func results in SC O(1)
+        // 1 : allow to visit
+        // 0 : can not visit due to obstacle
+        return 1 - obstacleGrid[y][x]
+    }
+    if m*n == 0 || allowToVisit(0, 0) == 0{
+        return 0
+    }
+    obstacleGrid[0][0] = 1
+
+    for i := 1 ; i < m; i++{
+        obstacleGrid[i][0]= obstacleGrid[i-1][0] * allowToVisit(0, i)
+    }
+
+    for j := 1; j < n; j++{
+        obstacleGrid[0][j] = obstacleGrid[0][j-1] * allowToVisit(j, 0)
+    }
+
+    for i := 1; i < m ; i++{
+        for j := 1; j < n; j++{
+            obstacleGrid[i][j] = (obstacleGrid[i-1][j] + obstacleGrid[i][j-1]) *allowToVisit(j, i)
+        }
+    }
+    return obstacleGrid[m-1][n-1]
+}
