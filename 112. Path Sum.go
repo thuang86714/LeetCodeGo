e37ed54789f74a6acdc7b7a96115fc02fa07c240
thuang86714 <thuang0714@gmail.com>
2024-02-27 00:15:09 +0800

    Q35, Q112

---
+++ 112. Path Sum.go
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func hasPathSum(root *TreeNode, targetSum int) bool {
+    //DFS, TC O(n), SC O(logn)
+    if root == nil{
+        return false
+    }
+    targetSum -= root.Val
+    if targetSum == 0 && root.Left == nil && root.Right == nil{
+        return true
+    }
+    return hasPathSum(root.Left, targetSum) || hasPathSum(root.Right, targetSum)
+}
---
+++ 35. Search Insert Position.go
@@ -0,0 +1,15 @@
+func searchInsert(nums []int, target int) int {
+	//binary search, TC O(logn)-->since it's a sorted slice, SC O(1)
+    left, right := 0, len(nums) - 1
+    for left <= right{
+        mid := right + (left - right)/2
+        if nums[mid] == target{
+            return mid
+        }else if nums[mid] < target{
+            left = mid + 1
+        }else{
+            right = mid - 1
+        }
+    }
+    return left
+}
